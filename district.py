214f1d0329ecc7d193675c7f225db1adf9640d8b
dijia478 <dijia478@163.com>
2022-08-11 18:34:36 +0800

    请求url

---
+++ district.py
@@ -0,0 +1,16 @@
+district_map = {
+    'beilin': '碑林区',
+    'weiyang': '未央区',
+    'baqiao': '灞桥区',
+    'xinchengqu': '新城区',
+    'lintong': '临潼区',
+    'yanliang': '阎良区',
+    'changan7': '长安区',
+    'lianhu': '莲湖区',
+    'yanta': '雁塔区',
+    'huyiqu': '鄠邑区',
+    'gaoling1': '高陵区',
+    'lantian': '蓝天县',
+    'zhouzhi': '周至县',
+    'xixianxinquxian': '西咸新区'
+}
---
+++ main.py
@@ -0,0 +1,33 @@
+import requests
+
+import district
+import ua_pool
+import random
+from bs4 import BeautifulSoup
+
+# 计算总页数
+def get_total_page_num(total_count: int, page_size: int):
+    return int((total_count + page_size - 1) / page_size)
+
+# 初始化ua池
+ua_pool.init()
+
+# 发起请求
+district_map = district.district_map
+for key, value in district_map.items():
+    total_page_num = 100
+    for page_num in range(total_page_num):
+        url = 'https://xa.ke.com/ershoufang/' + key + '/pg' + str(page_num + 1) + '/'
+        header = {'User-Agent': random.choice(ua_pool.ua_info_list)}
+        request = requests.get(url, headers=header)
+
+        # 解析html
+        soup = BeautifulSoup(request.text, 'lxml')
+        house_list = soup.find_all('li', class_='clear')
+        for house_info in house_list:
+            title = house_info.find('div', class_='title').find('a').string
+            positionInfo = house_info.find('div', class_='positionInfo').find('a').string
+            print(title + positionInfo)
+
+
+
