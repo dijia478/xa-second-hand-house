5471b7709e270a07b0c73c6c96f61a7a1890d3d3
dijia478 <dijia478@163.com>
2022-08-13 09:27:30 +0800

    添加sql

---
+++ second_hand_house.sql
@@ -0,0 +1,27 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for second_hand_house
+-- ----------------------------
+DROP TABLE IF EXISTS `second_hand_house`;
+CREATE TABLE `second_hand_house`  (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '主键id',
+  `project_name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '项目名称',
+  `district` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '区域',
+  `house_type` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '房屋户型',
+  `build_date` char(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '建成年份',
+  `release_date` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '发布日期',
+  `follower_num` int(11) NULL DEFAULT NULL COMMENT '关注人数',
+  `buy_area` decimal(10, 2) NULL DEFAULT NULL COMMENT '建筑面积',
+  `floor_num` char(6) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '楼层',
+  `total_floor_num` tinyint(4) UNSIGNED NULL DEFAULT NULL COMMENT '总楼层',
+  `unit_price` decimal(10, 2) NULL DEFAULT NULL COMMENT '单价',
+  `total_price` decimal(10, 2) NULL DEFAULT NULL COMMENT '总价',
+  `create_time` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  `update_time` datetime(0) NULL DEFAULT NULL COMMENT '修改时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_project_name`(`project_name`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
